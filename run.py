ad0565fc5de3f3f9eeb8c5cc3293c2c4097939cd
Askerin <134921117+Cabel7@users.noreply.github.com>
2024-07-16 16:56:04 +0530

    Update run.py

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ app = modal.App("jupyter")
         pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118 && \
         pip install -q xformers==0.0.20 triton==2.0.0 packaging==23.1 notebook"
     ),
+    network_file_systems={"/content"},
     gpu="T4",
     timeout=60000,
 )
