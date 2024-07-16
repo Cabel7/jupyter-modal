588114a6e745062c31759e097b3fffb0b86c0ad2
Askerin <134921117+Cabel7@users.noreply.github.com>
2024-07-16 19:33:07 +0530

    Update run.py

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ vol = modal.Volume.from_name("jupyter", create_if_missing=True)
         pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118 && \
         pip install -q xformers==0.0.20 triton==2.0.0 packaging==23.1 notebook"
     ),
-    volume={"/content": vol},
+    volumes={"/content": vol},
     gpu="T4",
     timeout=60000,
 )
